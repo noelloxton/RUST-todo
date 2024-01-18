b5f5d3b657387aaaa7bba2eb7ca647974b2f32e8
noelloxton <noeljloxton@gmail.com>
2024-01-18 14:25:29 +0200

    added cors to main

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,24 @@ mod model;
 mod response;
 mod route;
 
-use axum::{response::IntoResponse, routing::get, Json, Router};
-
-async fn health_check_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "Endpoint is healthy!";
-
-    let json_response = serde_json::json!({
-        "status": "success",
-        "message": MESSAGE
-    });
-
-    Json(json_response)
-}
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
+use route::create_router;
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api/healthchecker", get(health_check_handler));
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
-    println!("Server started successfully at port 8000");
+    let app = create_router().layer(cors);
 
+    println!("🚀 Server started successfully");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
