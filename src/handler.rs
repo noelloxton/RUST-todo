ca38d81b7e330c4e55e2f6cf2b70ccb5f6a9ab7a
noelloxton <noeljloxton@gmail.com>
2024-01-18 14:24:46 +0200

    moved healthcheck to handler file

--- src/handler.rs
+++ src/handler.rs
@@ -11,6 +11,17 @@ use crate::{
     response::{SingleTodoResponse, TodoData, TodoListResponse},
 };
 
+pub async fn health_checker_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "Build Simple CRUD API in Rust using Axum";
+
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
+}
+
 pub async fn todos_list_handler(
     opts: Option<Query<QueryOptions>>,
     State(db): State<DB>,
